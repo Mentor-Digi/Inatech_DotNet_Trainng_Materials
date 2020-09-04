66da26a3728be3251a8c0d66b77de24d04bec48b
Mentor-Digi <62875230+Mentor-Digi@users.noreply.github.com>
2020-09-04 13:32:04 +0530

    Add files via upload

---
+++ Day 5 - 4th Sep 2020/ValidExp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FWClasses
+{
+    class ValidExp
+    {
+        public static void Main()
+        {
+            String str = "Peacock";
+            Console.WriteLine(str.Length);
+            Console.WriteLine(str.Substring(3, 4));
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine(str.ToLower());
+            Console.WriteLine(str.Replace("cock", "hen"));
+            Console.WriteLine(str.Replace("c", "d"));
+            //Prod001
+            //if "Yes" "Male" "male"
+            //Production - prod prd 
+            //Matching Pettern
+            //Regex -Java Script - Client - Client Side Validation
+            //patterns - universal
+            str = "It is Simply a South Special dish";
+            MatchCollection match = Regex.Matches(str, @"\bS\S*");
+            foreach (Match m in match)
+                Console.WriteLine(m);
+
+            str = "Money and material is not everything that makes life";
+            MatchCollection match1= Regex.Matches(str, @"\bm\S*s\b");
+            foreach (Match m in match1)
+                Console.WriteLine(m);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day 5 - 4th Sep 2020/ValidExp2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FWClasses
+{
+    class ValidExp2
+    {
+        public static void Main()
+        {
+            string str = "Pattern Matching with Regular Expressions";
+            string pattern = "\\s+";
+            string rept = "-";
+            Regex rgx = new Regex(pattern);
+            string res = rgx.Replace(str, rept);
+            Console.WriteLine(str);
+            Console.WriteLine(res);
+            Console.Read();
+        }
+    }
+}
